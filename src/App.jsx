93370c4633a63a375b3582df7b0cf9ff2c4f7351
Joey Ovey <josephovey6@gmail.com>
2026-01-04 14:17:33 +0100

    Created Main home page that links other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import HomeCards from "./components/HomeCards"
-import JobListings from "./components/JobListings"
-import ViewAllJobs from "./components/viewAllJobs"
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<HomePage />} />)
+)
 
 const App = () => {
   return (
-    <>
-    <Navbar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs />
+    <RouterProvider router={router}/>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
