9d1107bf8da3b125766f13f5141a17474ac5029f
Joey Ovey <josephovey6@gmail.com>
2026-01-04 14:26:14 +0100

    imported react Outlet to display all the pages

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Outlet />
+    </>
   )
 }
 
