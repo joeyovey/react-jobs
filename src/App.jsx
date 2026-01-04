d306fe6bb8c0e23d6b17a468647e4be220b8733e
Joey Ovey <josephovey6@gmail.com>
2026-01-04 17:11:24 +0100

    Updated the index page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
+import AddJobs from "./pages/AddJobs";
 
 
 const router = createBrowserRouter(
@@ -14,6 +15,7 @@ const router = createBrowserRouter(
   <Route path="/" element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path="/jobs" element={<JobsPage />} />
+    <Route path="/add-job" element={<AddJobs />} />
   </Route>
   )
 )
