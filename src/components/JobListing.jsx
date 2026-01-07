8b69c8c8cc2590ef519f4ee13d35d83e86cd50b5
Joey Ovey <josephovey6@gmail.com>
2026-01-07 03:49:05 +0100

    mend

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { FaMapMarker } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const JobListing = ({ job }) => {
     const [fullDescription, setFullDescription] = useState(false);
@@ -32,7 +33,7 @@ const JobListing = ({ job }) => {
                         <FaMapMarker className="inline text-lg mb-1 mr-1"/>
                         {job.location}
                     </div>
-                    <Linkgi
+                    <Link
                         to={`/job/${job.id}`}
                         className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
                         Read More
